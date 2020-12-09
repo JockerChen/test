376aed681d9e38fff0baea9a3d7311f67f35faff
JokerChen <525705420@qq.com>
2020-12-09 12:44:11 +0800

    路由

--- node/serverroute.js
+++ node/serverroute.js
@@ -2,19 +2,15 @@
  * @Author: [JokerChen]
  * @Date: 2020-12-08 20:02:26
  * @LastEditors: [JokerChen]
- * @LastEditTime: 2020-12-08 20:27:23
+ * @LastEditTime: 2020-12-09 12:42:28
  * @Description: 路由功能
  */
 const express = require('express');
-
 let server=express();
-
 //创建路由
 let routerUser=express.Router();
-
 //将路由加入到express对象中
 server.use('/user',routerUser);
-
 //获取相关的路由信息进行配置相应的功能及返回项目
 routerUser.get('/1.html',(req,res)=>{
   res.send('user1');
@@ -24,14 +20,14 @@ routerUser.get('/2.html',(req,res)=>{
 })
 server.listen(8083);
 
-let routerActicle=express.Router();
-server.use('/acticle',routerActicle);
+let routerArticle=express.Router();
+server.use('/article',routerArticle);
 
-routerActicle.get('/1.html',(req,res)=>{
-  res.send('userActicle1');
+routerArticle.get('/1.html',(req,res)=>{
+  res.send('userArticle');
 })
 
 //读取到2相关的信息
-routerActicle.get('/2.html',(req,res)=>{
+routerArticle.get('/2.html',(req,res)=>{
   res.send('userActicle1');
 })
