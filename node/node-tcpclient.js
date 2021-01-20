60511f1ac3b8ca059ed219e6893595e6ee05f181
JokerChen <525705420@qq.com>
2021-01-20 17:01:18 +0800

    更新代码

--- node/node-tcpclient.js
+++ node/node-tcpclient.js
@@ -7,26 +7,40 @@
  */
 const net = require('net');
 const client = net.createConnection({
-    host: '127.0.0.1',
-    port: 3000
+    host: 't60.yunyibao.com.cn',
+    port: 20010
 });
 
 client.on('connect', () => {
+    var myDate = new Date();
+    console.log("发送", myDate.toLocaleTimeString());
+    console.time("发送");
+    let jsonInfo = `
+    {
+        "b":
+        {
+            "img":"",
+            "tw":"34.6",
+            "idcard":"",
+            "name":"陈宁",
+            "term_id":"00011800202007001148",
+            "type":"1",
+            "healthCardid":"397D143BC0733905DFFF686D1C86F4059C9CB3E62D22E613FD38A8B1C6BFE2B0:0:4A0BC609CC42AC1541DC392AD048AB82"
+        },
+        "h":
+        {
+            "secretkey":"DCABCCBC4C1B7540737B6F5D2B8E7DC32FB65A10FCB3F470C50B95884416004A"
+            ,"app_id":"89BC1361C429487494EA047CF85739BA"
+        }
+    }`;
     // 向服务器发送数据
-    client.write('Nodejs 技术栈');
-
-    setTimeout(() => {
-        client.write('JavaScript ');
-        client.write('TypeScript ');
-        client.write('Python ');
-        client.write('Java ');
-        client.write('C ');
-        client.write('PHP ');
-        client.write('ASP.NET ');
-    }, 1000);
+    client.write(jsonInfo);
+    console.timeEnd("发送");
 })
 
 client.on('data', buffer => {
+    var myDate = new Date();
+    console.log("接收", myDate.toLocaleTimeString());
     console.log(buffer.toString());
 });
 
